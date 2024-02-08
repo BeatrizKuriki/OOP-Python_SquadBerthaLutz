fc1de15e2c77bacf51106feaf8ab2216934d8662
Cíntia Brito <134309478+CinBrito@users.noreply.github.com>
2024-02-08 17:44:34 -0300

    Update autor.py

--- biblioteca/pessoas/autor.py
+++ biblioteca/pessoas/autor.py
@@ -1,5 +1,6 @@
 from pessoas.pessoa import Pessoa
 
 class Autor(Pessoa):
-    def __init__(self, nome, nacionalidade):
+    def __init__(self, id, nome, nacionalidade):
         super().__init__(nome, nacionalidade)
+        self.id = id
