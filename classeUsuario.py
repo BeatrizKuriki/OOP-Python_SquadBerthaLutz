fe88936056cb54cfda7364fa62e2befb1b8ab0a1
Karolina Meira <74688759+KNMeira@users.noreply.github.com>
2024-02-08 17:49:07 -0300

    Update classeUsuario.py

--- classeUsuario.py
+++ classeUsuario.py
@@ -1,10 +1,12 @@
+from pessoas.pessoa import Pessoa
 class Usuario:
     def __init__(self, nome, telefone, nacionalidade):
         self.nome = nome
         self.telefone = telefone
         self.nacionalidade = nacionalidade
 
-user1 = Usuario('Karol', '23659874', 'Brasileira')
+user1 = Usuario('23658', 'Karol', '23659874', 'Brasileira')
+print('id:', user1.id)
 print('Nome:', user1.nome)
 print('Telefone:', user1.telefone)
 print('Nacionalidade:', user1.nacionalidade)
