4f4392c7612238c577eb3aa95774dbee4219815e
Beatriz Alencar Kuriki <beatriz.alencar@sesatech.com.br>
2024-02-12 15:00:37 -0300

    change: pacote pessoas

--- biblioteca/pessoas/pessoa.py
+++ biblioteca/pessoas/pessoa.py
@@ -1,9 +1,11 @@
+from abc import ABC, abstractmethod
 
-
-
-class Pessoa():
+class Pessoa(ABC):
      def __init__(self, nome, telefone, nacionalidade):
         self._nome = nome
         self._telefone = telefone
         self._nacionalidade = nacionalidade
+
+
+      def   
     
